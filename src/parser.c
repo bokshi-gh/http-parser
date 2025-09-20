5cc158555405a1c427523f8666a6567e170ab1ff
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-09-21 02:43:58 +0545

    add get header

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,6 @@
 #include "../include/parser.h"
+#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 HTTPRequest parseHTTPRequest(char *raw_request){
@@ -15,4 +17,24 @@ HTTPRequest parseHTTPRequest(char *raw_request){
 	parsed_request.body = body_start;
 
 	return parsed_request;
+}
+
+char* getHeader(char *raw, char *name){
+	char *header_line = strstr(raw, name);
+
+	if(header_line == NULL) return NULL;
+
+	char *colon = strchr(header_line, ':');
+	char *value_start = colon + 1;
+	while (*value_start == ' ') value_start++;
+	char *value_end = strstr(header_line, "\r\n");
+	size_t value_len = value_start - value_end;
+
+	char *value = (char*)malloc(value_len + 1);
+	if (value == NULL) return NULL;
+
+	strncpy(value, value_start, value_len);
+	value[value_len] = '\0';
+
+	return value;
 }
