05ad7f81e0ec47cc8d14da614dc886a56ae41dab
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-09-21 02:20:31 +0545

    add request parser

--- include/parser.h
+++ include/parser.h
@@ -1,22 +1,15 @@
 #pragma once
-#define MAX_HEADERS 32
-#define MAX_NAME 64
-#define MAX_VALUE 256
 
 typedef struct {
-    char name[MAX_NAME];
-    char value[MAX_VALUE];
-} Header;
-
-typedef struct {
-    char *raw;
-    char *method;
-    char *path;
-    char *http_version;
-    char *body;
-    Header headers[MAX_HEADERS];
-    int header_count;
+	char *raw;
+	char *method;
+	char *path;
+	char *http_version;
+	char *body;
+	
 } HTTPRequest;
 
-HTTPRequest parseHTTPRequest(char *raw);
-char* getHeader(HTTPRequest *request, const char *name);
+HTTPRequest parseHTTPRequest(char *raw_request);
+HTTPRequest parseHTTPResponse(char *raw_response);
+char* getHeader(char *raw, char *name);
+
